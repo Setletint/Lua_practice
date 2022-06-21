452f63272a168daaae8d468584acf6c20e1d9577
Setletint <72040591+Setletint@users.noreply.github.com>
2022-06-21 21:46:16 +0300

    Update startup.lua

--- Startup_conf_comm/lua/autorun/startup.lua
+++ Startup_conf_comm/lua/autorun/startup.lua
@@ -1,8 +1,25 @@
 if SERVER then
+	--[[
 	include('server/serverlua.lua')
 	AddCSLuaFile('client/clientlua.lua')
+	]]
+	local files = file.Find('server/*.lua', 'LUA')
+	table.foreach(files, function(key, gui)
+		include('server/' .. gui)
+	end )
+	local files = file.Find('client/*.lua', 'LUA')
+	table.foreach(files, function(key, gui)
+		AddCSLuaFile('client/' .. gui)
+	end)
 end
 
 if CLIENT then
-	include('client/clientlua.lua')
+--	include('client/clientlua.lua')
+
+local files = file.Find('client/*.lua', 'LUA')
+	table.foreach(files, function(key, gui)
+		include('client/' .. gui)
+	end )
 end
+
+print('StartUp sucsesfully loaded!')
